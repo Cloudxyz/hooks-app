7a6b90cfa3d15bdd2defe061249eff543f5337bd
Alan Cordova <info.devalan@gmail.com>
2023-01-12 01:30:04 -0600

    Start Reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { Padre } from './07-tarea-memo/Padre'
+// import { MemorizeHook } from './06-memos/MemorizeHook'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -9,6 +13,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <Padre />
   </React.StrictMode>,
 )
