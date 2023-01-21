b6f0a9aaf13bd77bde6aff5deb835c90d8b2dd74
Alan Cordova <info.devalan@gmail.com>
2023-01-21 00:22:56 -0600

    UserContext

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { UserProvider } from './context/UserProvider'
 import { NavBar } from './NavBar'
 import { AboutPage, HomePage, LoginPage } from './pages'
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <h1>MainApp</h1>
             <NavBar />
             <hr />
@@ -16,6 +17,6 @@ export const MainApp = () => {
 
                 <Route path="*" element={<Navigate to="/about"/>} />
             </Routes>
-        </>
+        </UserProvider>
     )
 }
--- src/09-useContext/pages/LoginPage.jsx
+++ src/09-useContext/pages/LoginPage.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext';
 
 export const LoginPage = () => {
+
+    const {user, setUser} = useContext(UserContext);
     return (
         <>
             <h1>Login Page</h1>
+            <h2>Usuario: {user?.name}</h2>
             <hr />
+            <button
+                className='btn btn-primary'
+                onClick={() => setUser({id: 123, name: 'Alan'})}
+            >
+                Usuario
+            </button>
         </>
     )
 }
